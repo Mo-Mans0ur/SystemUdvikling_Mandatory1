631f63e699d287f86d672ff886043cd042d966b4
Mo-Mansour <Moha199@outlook.dk>
2024-10-01 23:12:16 +0200

    selenium test done

--- personal-test-data-generator/src/app.js
+++ personal-test-data-generator/src/app.js
@@ -1,16 +1,27 @@
-import express from "express";
-import personRoutes from "./routes/personRoutes.js";
+import express from 'express';
+import personRoutes from './routes/personRoutes.js';
+import path from 'path';
 
-const app = express();
-app.use(express.json());
+const app = express(); // Create the Express app
+app.use(express.json()); // Middleware to handle JSON
+
+// Serve static files from the 'public' folder
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CORS middleware (place this right after initializing the app)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // Route to get a random person
 app.use("/api/person", personRoutes);
 
-// basic route to test if the server is running
+// Basic route to test if the server is running
 app.get("/", (req, res) => {
-  res.send("Hi all, welcome to the personal test data generator!");
+    res.send("Hi all, welcome to the personal test data generator!");
 });
-
 
 export default app;
